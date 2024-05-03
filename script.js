4a0b1b0a4e2710a3d52c8cf6d3a086b5f1e53c20
phocuz <victorishola580@gmail.com>
2024-05-03 13:34:15 +0100

    added some text and javascript scroll eventlistener

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
 const hamburger=document.querySelector('.hamburger-icon');
 const menuLines= document.querySelector('.menu-lines');
+const scrollElement = document.querySelector('.nav-header,.logo,.header-list');
 
 hamburger.addEventListener('click',()=>{
     menuLines.classList.toggle('active');
     hamburger.classList.toggle('active');
 })
-window.addEventListener('click' ,()=>{
+document.addEventListener("scroll", () => {
+    const scroll_position = window.scroll;
+    if (scroll_position > 250) {
+      scrollElement.style.backgroundColor = "#00ff4";
+    } else {
+     scrollElement.backgroundColor = "transparent";
+    }
+  });
 
-})
